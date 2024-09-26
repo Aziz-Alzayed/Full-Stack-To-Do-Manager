3d55b9a89b68a746c363ec156db7d5e81ba07c29
Aziz-Alzayed <67501412+Aziz-Alzayed@users.noreply.github.com>
2024-09-26 09:56:33 +0300

    update terraform

--- terraform/modules/linux_app_service/main.tf
+++ terraform/modules/linux_app_service/main.tf
@@ -10,14 +10,11 @@ resource "azurerm_linux_web_app" "linux_app_service" {
     "DOCKER_REGISTRY_SERVER_USERNAME" = var.docker_registry_server_username
     "DOCKER_REGISTRY_SERVER_PASSWORD" = var.docker_registry_server_password
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "DOCKER_CUSTOM_IMAGE_NAME"     = var.docker_image 
+    "DOCKER_CUSTOM_IMAGE_NAME"     = "${var.docker_image}:${var.docker_image_tag}"
   }
 
   site_config {
-    application_stack {
-        docker_image            = var.docker_image
-        docker_image_tag        = var.docker_image_tag
-    }
+
   }
 
   identity {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,3 @@
-# -------------------------------------------------------------------------------------
-# Terraform Variables for Azure Configuration
-# -------------------------------------------------------------------------------------
-# This file defines the input variables required for authenticating and interacting 
-# with Azure resources via the AzureRM provider.
-#
-# These variables include:
-# - Azure Subscription ID, Client ID, Client Secret, and Tenant ID:
-#   These are the credentials needed to authenticate Terraform with Azure using a 
-#   Service Principal.
-# - SQL Server Login and Password:
-#   These are credentials used to create or manage an Azure SQL Server instance.
-#
-# All of these variables are marked as sensitive to ensure that they are not displayed
-# in the Terraform output. These values should be securely passed through a .tfvars
-# file or environment variables.
-#
-# Usage:
-# 1. Define these variables in a secrets.tfvars file or pass them via environment 
-#    variables.
-# 2. Do not hardcode sensitive values in the configuration files to maintain security.
-#
-# Example tfvars file:
-# subscription_id           = "your-subscription-id"
-# client_id                 = "your-client-id"
-# client_secret             = "your-client-secret"
-# tenant_id                 = "your-tenant-id"
-# sql_server_login          = "sqladmin"
-# sql_server_login_password = "your-secure-password"
-# -------------------------------------------------------------------------------------
 
 variable "subscription_id" {
   type        = string
